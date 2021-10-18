6ec21636a124ad09339965f7f8f2291e58a6161e
LooFig <lorrannyfigueredo@gmail.com>
2021-10-18 20:21:53 -0300

    Commit inicial

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ function carregar () {
     let horas = data.getHours() >= 9 ? data.getHours() : `0${data.getHours()}`;
     let minutos = data.getMinutes() >= 9 ? data.getMinutes() : `0${data.getMinutes()}`;
 
-    res.innerHTML = `<p>Agora são ${horas} horas e ${minutos} minutos.`;
+    res.innerHTML = `<p>Agora são ${horas}:${minutos}.`;
 
     // Verificação do horário
-    if (horas >= 0 && horas < 12) {
+    if (horas >= 00 && horas < 6) {
+        img.src = 'fotonoite.png';
+        document.body.style.background = '#515154';
+        res.innerHTML += `<strong>Boa madrugada!`;
+    } else if (horas < 12){
         img.src = 'fotomanhã.png';
         document.body.style.background = '#e2cd9f';
-        res.innerHTML += `<strong>Bom dia!`;
+        res.innerHTML += `<strong>Bom dia!`;         
     } else if (horas < 18){
         img.src = 'fototarde.png';
         document.body.style.background = '#b9846f';
-        res.innerHTML += `<strong>Boa tarde!`;           
+        res.innerHTML += `<strong>Boa tarde!`;
     } else {
         img.src = 'fotonoite.png';
         document.body.style.background = '#515154';
